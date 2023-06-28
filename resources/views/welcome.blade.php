48c0cd5e56744848cef076335c58d183612bbc31
alessandromarangoni <alessandromarangoni980@gmail.com>
2023-06-28 18:54:00 +0200

    add data on welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,12 +2,24 @@
 
 @section('content')
 <div class="container my-3">
-    <h1>Welcome Page</h1>
+    <h1>Most Viewed</h1>
     <div class="row g-4">
         <div class="col">
-            <div>
+            <div class="d-flex justify-content-between  flex-wrap">
                 @foreach ($movies as $item)
-                    {{$item -> title}}
+                    <div class="card my-3 mx-1 w-25">
+                        <div class="card-header">
+                            <h2>{{$item -> title}}</h2>
+                        </div>
+                        <div class="card-body">
+                            <ul class="list-group">
+                                <li class="list-group-item"><span><strong>Original Title: </strong></span> {{$item->original_title}}</li>
+                                <li class="list-group-item"><span><strong>Nationality: </strong></span>:  {{$item->nationality}}</li>
+                                <li class="list-group-item"><span><strong>Date: </strong></span> {{$item->date}}</li>
+                                <li class="list-group-item"><span><strong>Vote: </strong></span>:  {{$item->vote}}</li>
+                            </ul>
+                        </div>
+                    </div>
                 @endforeach
             </div>
         </div>
